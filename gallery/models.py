86d95c4f639e7116c6336be4ffce4ec5275373bc
ira <irakolil31@gmail.com>
2025-01-17 12:08:39 +0300

    Change

--- gallery/models.py
+++ gallery/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Image(models.Model):
-    title = models.CharField(max_length=255) 
-    description = models.TextField(blank=True)  
-    image = models.ImageField(upload_to='images/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')  # Store the image in 'images/' folder
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='liked_images', blank=True)
     dislikes = models.ManyToManyField(User, related_name='disliked_images', blank=True)
-
 
     def total_likes(self):
         return self.likes.count()
